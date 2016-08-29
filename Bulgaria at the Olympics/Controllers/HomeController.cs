7b85e3bed81acb91a6c0ff3442f89c367055e7e8
KostadinMarov <brazileca_23@abv.bg>
2016-08-29 21:45:52 +0300

    The Final part of my block!
    Only some work to the end.

--- Bulgaria at the Olympics/Controllers/HomeController.cs
+++ Bulgaria at the Olympics/Controllers/HomeController.cs
@@ -22,7 +22,81 @@ namespace Bulgaria_at_the_Olympics.Controllers
 
             return View();
         }
+        public ActionResult FirstOlimpics()
+        {
+            ViewBag.Message = "Your application description page.";
 
-     
+            return View();
+        }
+
+        public ActionResult SecondOlimpycs()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult ThirdOlimpycs()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Berlin1936()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Helsinki1952()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Melbourne1956()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Rome1960()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Tokio1964()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Mexico1968()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Munich1972()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Monreal1976()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
     }
 }
